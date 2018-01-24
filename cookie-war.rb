4ffb277a98501bfbe5795b26333f1eb55ead6a5a
Barrett Cervenka <barrett@cervenka.com>
2018-01-24 08:28:26 -0600

    init

---
+++ cookie-war.rb
@@ -0,0 +1,28 @@
+require 'mechanize'
+require 'sqlite3'
+require 'yaml'
+require './parser'
+
+config = YAML.load_file 'cookie-war.yaml'
+
+if ENV['TEST']
+  p 'test'
+  available = File.open('available_page_sample.html') { |f| Nokogiri::HTML(f) }
+  parser = Parser.new
+  parser.parse available
+else
+  a = Mechanize.new
+  a.get('http://ohsucookies.com/auth/login') do |page|
+
+    my_page = page.form_with(:action => '/auth/login') do |form|
+      username_field = form.field_with(:name => "email")
+      username_field.value = auth.email
+      password_field = form.field_with(:name => "password")
+      password_field.value = auth.password
+    end.click_button
+
+    available = my_page.link_with(href: /available/).click()
+    parser = Parser.new
+    parser.parse available
+  end
+end
---
+++ parser.rb
@@ -0,0 +1,74 @@
+require 'nokogiri'
+require 'sqlite3'
+require 'twilio-ruby'
+require 'logger'
+require 'yaml'
+
+class Parser
+  HIT_LIST = [
+    /Chuy/,
+    /Cypress Grill/,
+    /Flores/,
+    /Great Clips/,
+    /Jack Allen/,
+    /Kerby Lane/,
+    /Randall/,
+    /Serranos/,
+    /Torchy/,
+    /Via 313/,
+    /Walgreens/,
+    /Walmart/,
+    /Waterloo/
+  ]
+
+  def initialize
+    @log = Logger.new 'cookie-war.log'
+    config = YAML.load_file 'cookie-war.yaml'
+
+    # Twilio
+    account_sid = config['twilio']['account_sid']
+    auth_token = config['twilio']['auth_token']
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @to = config['twilio']['to']
+    @from = config['twilio']['from']
+
+    # db
+    @db = SQLite3::Database.new 'cookie-war.db'
+
+    @db.execute <<-SQL
+      CREATE TABLE IF NOT EXISTS cookies (
+        datetime text(30),
+        location varchar(100),
+        primary key(datetime, location)
+      );
+    SQL
+  end
+
+  def parse doc
+    @log.info 'BEGIN getting available cookie list'
+    re = Regexp.union(HIT_LIST)
+    doc.search('table').first.css('tr').each do |row|
+      date = row.css('td')[0].text.gsub!(/[\n\r\t]/, '')
+      times = row.css('td')[1].text.gsub!(/[\n\r\t\s]/, '')
+      time = times.split('-')[0]
+      location = row.css('td')[2].text.gsub!(/[\n\r\t]/, '')
+      datetime = DateTime.strptime(date + ' ' + time, '%m/%d/%Y %H:%M%P')
+      @log.info 'found ' + location + ' at ' + datetime.to_s
+      if location.match(re)
+        # it's a hit so see if we have a db entry
+        @log.info location + ' is a HIT'
+        begin
+          @db.execute('insert into cookies (datetime, location) values (?, ?)', [datetime.to_s, location])
+          body = 'Cookie alert: ' + location + ' ' + datetime.to_s
+          @log.info 'Sending alert ' + body
+          #@client.api.account.messages.create(from: config['twilio']['from'], to: config['twilio']['to'], body: body)
+        rescue SQLite3::ConstraintException => e
+          @log.debug 'constraint violation - already reported on ' + location + ' ' + datetime.to_s
+        end
+      else
+        @log.info 'ignoring ' + location
+      end
+    end
+
+  end
+end
